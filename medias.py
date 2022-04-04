ae3c3488c22769a944570b449acd5dd6b2557c53
caiocnc <82780666+caiocnc@users.noreply.github.com>
2022-04-04 14:45:30 -0300

    Version 0.1 - Semana 1
    Update das primeiras aulas que tive sobre Python.

---
+++ medias.py
@@ -0,0 +1,8 @@
+primeiraNota = input("Digite a primeira nota: ")
+segundaNota = input("Digite a segunda nota: ")
+terceiraNota = input("Digite a terceira nota: ")
+quartaNota = input("Digite a quarta nota: ")
+media = (float(primeiraNota)+float(segundaNota)+float(terceiraNota)+float(quartaNota))
+mediaAritmetica = media / 4
+print ("A média aritmética é" , mediaAritmetica)
+input()
